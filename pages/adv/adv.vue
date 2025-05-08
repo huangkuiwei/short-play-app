6ecbefe16ebfc84685e0178acad4f06fa770a38d
huangkuiwei <1017969390@qq.com>
2025-05-08 17:52:48 +0800

    提交

--- pages/adv/adv.vue
+++ pages/adv/adv.vue
@@ -7,14 +7,30 @@ export default {
   data() {
     return {
       advList: [],
-      currentAdv: {}
+      currentAdv: {},
+      swiperList: [],
+      totalMoney: Number(uni.getStorageSync('totalMoney')) || 0,
+      showNoticeDialog: false,
+      withdrawalMoney: '',
     }
   },
 
   onLoad() {
+    let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    for (let i = 0; i < 4; i++) {
+      let s = ''
+
+      for (let i = 0; i < 4; i++) {
+        s += numbers[Math.floor(Math.random() * numbers.length)]
+      }
+
+      this.swiperList.push(s)
+    }
+
     this.advList = []
 
-    new Array(60).fill(undefined).forEach((item, index) => {
+    new Array(9).fill(undefined).forEach((item, index) => {
       let moneyData = uni.getStorageSync('moneyData')[3]
       let money = Number(moneyData.minMoney) + Math.random() * (moneyData.maxMoney - moneyData.minMoney)
 
@@ -28,7 +44,7 @@ export default {
   },
 
   computed: {
-    ...mapState('app', ['deviceUuid']),
+    ...mapState('app', ['deviceUuid', 'noticeList']),
   },
 
   methods: {
@@ -38,9 +54,62 @@ export default {
       })
     },
 
+    goback() {
+      uni.navigateBack()
+    },
+
     lookAdv(item) {
       this.currentAdv = item
       this.$refs.advDialog.open()
+    },
+
+    withdrawal() {
+      if (Number(this.withdrawalMoney) < 1) {
+        uni.showToast({
+          title: '最低提现1',
+          icon: 'error'
+        })
+
+        return
+      }
+
+      if (Number(this.withdrawalMoney) > this.totalMoney) {
+        uni.showToast({
+          title: '余额不足',
+          icon: 'error'
+        })
+
+        return
+      }
+
+      uni.showLoading({
+        title: '请稍等...',
+        mask: true,
+      })
+
+      setTimeout(() => {
+        uni.showToast({
+          title: '提现成功',
+          icon: 'success'
+        })
+
+        this.$refs.withdrawalDialogRef.close()
+        this.totalMoney = Number((this.totalMoney - this.withdrawalMoney).toFixed(2))
+        uni.setStorageSync('totalMoney', this.totalMoney)
+
+        setTimeout(() => {
+          this.showNoticeDialog = true
+
+          this.noticeList.push({
+            time: Date.now(),
+            money: Number(this.withdrawalMoney).toFixed(2),
+          })
+
+          setTimeout(() => {
+            this.showNoticeDialog = false
+          }, 4000)
+        }, 4000)
+      }, 2000)
     },
 
     playEnded() {
@@ -48,15 +117,9 @@ export default {
 
       setTimeout(() => {
         this.$refs.successDialog.open()
+        this.totalMoney = (this.totalMoney + Number(this.currentAdv.money)).toFixed(2)
+        uni.setStorageSync('totalMoney', this.totalMoney)
       }, 500)
-
-      uni.request({
-        url: `http://110.40.131.58:5000/api/app-bind-pwd/addwithdrawrecords/${this.deviceUuid}/${this.currentAdv.money}/3`,
-        method: 'POST',
-        header:{
-          Authorization: `Bearer ${uni.getStorageSync('token')}`
-        },
-      })
     }
   }
 }
@@ -65,30 +128,85 @@ export default {
 <template>
   <view class="adv-page">
     <view class="tabs">
-      <view class="tab">热点</view>
-      <view class="tab">生活</view>
-      <view class="tab">商城</view>
-      <view class="tab">推荐</view>
-      <view class="tab">朋友</view>
-      <view class="tab">视频</view>
+      <view class="user">
+        <image mode="widthFix" src="/static/images/user-icon2.png" />
+        <view>用户：8896 欢迎回来</view>
+        <button size="mini" type="primary" @click="goback">退出</button>
+      </view>
+
+      <view class="options">
+        <view>
+          <text class="title">我的余额：{{ totalMoney }}</text>
+        </view>
+        <view @click="$refs.withdrawalDialogRef.open()">
+          <text class="title">点击提现</text>
+        </view>
+      </view>
+    </view>
+
+    <view class="swiper">
+      <swiper autoplay :interval="2000" :vertical="true">
+        <swiper-item v-for="(item, index) in swiperList" :key="index">
+          用户 <text>{{ item }}</text> 通过你的邀请进入了
+        </swiper-item>
+      </swiper>
+    </view>
+
+    <view class="hot">
+      <view class="hot-content">
+        <image mode="widthFix" src="/static/images/hot_fill.png" />
+        <text class="text1">热门推荐</text>
+        <text class="text2">查看></text>
+      </view>
     </view>
 
     <view class="adv-list">
       <view class="adv-item" v-for="item of advList" :key="item.name"  @click="lookAdv(item)">
         <view class="poster">
           <image class="cover" mode="aspectFill" :src="item.coverSrc"/>
-          <image class="play-icon" mode="widthFix" src="/static/images/play-circle.png"/>
+          <!--<image class="play-icon" mode="widthFix" src="/static/images/play-circle.png"/>-->
+          <button type="primary" size="mini">点击观看</button>
         </view>
 
         <view class="tip">
-          <text>观看视频，助力商家，得奖励！</text>
+          <text>看短剧，助力商家，得奖励！</text>
         </view>
 
         <view class="money">
           <text>￥</text>
           <text>{{item.money}}</text>
-          <text>剩余10000+</text>
         </view>
+
+        <view class="time-tip">
+          <text>观看10s即可完成任务领取奖励</text>
+        </view>
+      </view>
+    </view>
+
+    <uni-popup ref="withdrawalDialogRef" background-color="#ffffff" border-radius="5px 5px 5px 5px">
+      <view class="withdrawal-dialog">
+        <view class="title">提现</view>
+        <input type="number" v-model="withdrawalMoney" placeholder="请输入提现金额">
+        <view class="btn">
+          <button @click="$refs.withdrawalDialogRef.close()">
+            取消
+          </button>
+
+          <button type="primary" @click="withdrawal">
+            确定
+          </button>
+        </view>
+      </view>
+    </uni-popup>
+
+    <view class="notice-dialog" v-if="showNoticeDialog" @click="jumpUrl('/pages/noticeList/noticeList')">
+      <view class="left">
+        <image mode="widthFix" src="/static/images/horn.png"/>
+      </view>
+
+      <view class="right">
+        <text>微信支付</text>
+        <text>微信支付：您收到一笔活动奖励</text>
       </view>
     </view>
 
@@ -125,16 +243,15 @@ export default {
       </view>
     </uni-popup>
 
-    <view class="user-icon" @click="jumpUrl('/pages/userCenter/userCenter')">
-      <image mode="widthFix" src="/static/images/user-icon.png" />
-    </view>
+    <!--<view class="user-icon" @click="jumpUrl('/pages/userCenter/userCenter')">-->
+    <!--  <image mode="widthFix" src="/static/images/user-icon.png" />-->
+    <!--</view>-->
   </view>
 </template>
 
 <style lang="scss">
 page {
-  background: #ffffff;
-  padding: 100rpx 0 0;
+  background: #eeeeee;
   height: 100%;
   overflow: hidden;
 }
@@ -146,39 +263,112 @@ page {
   flex-direction: column;
 
   .tabs {
-    flex-shrink: 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    padding: 0 0 20rpx;
-    margin-bottom: 40rpx;
-    border-bottom: 1px solid #D0D3DE;
-    overflow: auto;
+    padding: 100rpx 0 0;
+    margin-bottom: 20rpx;
+    background: #fff;
 
-    &::-webkit-scrollbar {
-      display: none;
+    .user {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 40rpx;
+      margin-bottom: 40rpx;
+
+      image {
+        width: 60rpx;
+      }
+
+      view {
+        color: #555;
+        font-size: 40rpx;
+        font-weight: bold;
+      }
+
+      button {
+        margin: 0;
+      }
     }
 
-    .tab {
-      flex-shrink: 0;
-      font-size: 32rpx;
-      color: #000000;
+    .options {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 20rpx;
+      margin-bottom: 40rpx;
 
-      &:nth-child(6) {
-        position: relative;
-        color: #0062FF;
+      view {
+        width: 44%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100rpx;
+        color: #ffffff;
 
-        &:after {
-          content: '';
-          position: absolute;
-          bottom: -20rpx;
-          left: -30rpx;
-          width: 120rpx;
-          height: 4rpx;
-          background: #0062FF;
-          border-radius: 4rpx;
+        &:nth-child(1) {
+          background: #4cd964;
+        }
+
+        &:nth-child(2) {
+          background: #e43d33;
+        }
+
+        .title {
+          font-style: italic;
         }
       }
+    }
+  }
+
+  .swiper {
+    padding: 0 20rpx;
+    margin-bottom: 20rpx;
+
+    swiper {
+      background: #fff;
+      border-radius: 16rpx;
+      padding: 14rpx 0;
+      height: 60rpx;
+
+      swiper-item {
+        font-size: 28rpx;
+        padding-left: 40rpx;
+
+        text {
+          color: #e43d33;
+        }
+      }
+    }
+  }
+
+  .hot {
+    padding: 0 20rpx;
+    margin-bottom: 20rpx;
+
+    .hot-content {
+      background: #fff;
+      border-radius: 16rpx;
+      display: flex;
+      align-items: center;
+      padding: 10rpx 20rpx;
+    }
+
+    image {
+      width: 40rpx;
+      margin-right: 20rpx;
+    }
+
+    .text1 {
+      font-style: italic;
+      font-weight: bold;
+      flex-grow: 1;
+      font-size: 28rpx;
+    }
+
+    .text2 {
+      font-style: italic;
+      font-weight: bold;
+      color: #cccccc;
+      font-size: 24rpx;
     }
   }
 
@@ -213,12 +403,12 @@ page {
           height: 100%;
         }
 
-        .play-icon {
+        button {
           position: absolute;
           left: 50%;
           top: 50%;
           transform: translate(-50%, -50%);
-          width: 76rpx;
+          font-size: 20rpx;
         }
       }
 
@@ -228,7 +418,8 @@ page {
         flex-direction: column;
         justify-content: center;
         gap: 10rpx;
-        margin: 14rpx 0 16rpx;
+        margin: 14rpx 0 6rpx;
+        padding-left: 10rpx;
 
         text {
           &:nth-child(1) {
@@ -242,6 +433,8 @@ page {
         color: #FF4F42;
         display: flex;
         align-items: center;
+        margin-bottom: 10rpx;
+        padding-left: 10rpx;
 
         text {
           font-size: 28rpx;
@@ -250,12 +443,14 @@ page {
             font-weight: bold;
             flex-grow: 1;
           }
-
-          &:nth-child(3) {
-            color: #999999;
-            font-size: 20rpx;
-          }
         }
+      }
+
+      .time-tip {
+        color: #999999;
+        font-size: 20rpx;
+        padding-left: 10rpx;
+        padding-bottom: 10rpx;
       }
     }
   }
@@ -320,6 +515,117 @@ page {
 
     image {
       width: 50rpx;
+    }
+  }
+}
+
+.withdrawal-dialog {
+  width: 550rpx;
+  padding: 40rpx 50rpx;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .title {
+    text-align: center;
+    font-size: 40rpx;
+    font-weight: bold;
+    margin-bottom: 40rpx;
+  }
+
+  input {
+    height: 86rpx;
+    margin-bottom: 40rpx;
+    width: 100%;
+    border: 1px solid #cccccc;
+    border-radius: 16rpx;
+    padding: 0 30rpx;
+    font-size: 36rpx;
+  }
+
+  .btn {
+    align-self: stretch;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    button {
+      width: 180rpx;
+      height: 70rpx;
+      line-height: 70rpx;
+      font-size: 30rpx;
+    }
+  }
+}
+
+.notice-dialog {
+  display: flex;
+  align-items: center;
+  background: #ffffff;
+  position: fixed;
+  z-index: 999;
+  left: 40rpx;
+  right: 40rpx;
+  border-radius: 8rpx;
+  padding: 30rpx 30rpx;
+  top: 0;
+  opacity: 0;
+  transition: all 0.3s ease;
+  animation: 4s ease kf1;
+
+  @keyframes kf1 {
+    0% {
+      top: 0;
+      opacity: 0;
+    }
+
+    10% {
+      top: 100rpx;
+      opacity: 1;
+    }
+
+    90% {
+      top: 100rpx;
+      opacity: 1;
+    }
+
+    100% {
+      top: 0;
+      opacity: 0;
+    }
+  }
+
+  .left {
+    background: #00C800;
+    width: 90rpx;
+    height: 90rpx;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 40rpx;
+
+    image {
+      width: 50rpx;
+    }
+  }
+
+  .right {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10rpx;
+
+    text {
+      &:nth-child(1) {
+        font-size: 32rpx;
+        font-weight: bold;
+      }
+
+      &:nth-child(2) {
+        font-size: 28rpx;
+      }
     }
   }
 }
--- store/modules/app.js
+++ store/modules/app.js
@@ -5,13 +5,17 @@ const appStore = {
     // TODO h5测试数据
     // deviceUuid: '123456789',
     deviceUuid: '',
-    receiveRecode: []
+    noticeList: []
   },
 
   mutations: {
     _setDeviceUuid(state, payload) {
       state.deviceUuid = payload
     },
+
+    _setNoticeList(state, payload) {
+      state.noticeList = payload
+    }
   },
 
   actions: {},
